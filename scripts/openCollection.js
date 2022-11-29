12a501c075923d4469bdbdd37444e7076afba0b8
nvs99bn <nvs99bn@gmail.com>
2022-11-29 20:49:59 +0700

    .

--- scripts/openCollection.js
+++ scripts/openCollection.js
@@ -1,11 +1,19 @@
-let getElements = document?.getElementsByClassName(
-  "showcase__link js-detail-data-link"
-);
-if (getElements) {
-  for (let e of getElements) {
-    if (e) {
-      let linkCollection = e?.getAttribute("href");
-      window.open(linkCollection);
-    }
-  }
+// let a = [];
+// let getLinks = document
+//   ?.querySelectorAll("figure")
+//   .forEach((link) => a.push(link?.getAttribute("data-image")));
+// localStorage.setItem("links", a);
+// let b = [];
+// document
+//   .querySelectorAll("a > span")
+//   .forEach((a) => b.push(a.innerHTML === "Next page"));
+// console.log(b)
+// let oldData = localStorage.getItem("links").split(",")
+let linkNextPage = document
+  .getElementsByClassName(
+    "pagination__next button floatl pagination--static pd-y-none-i"
+  )[0]
+  ?.getAttribute("href");
+if(linkNextPage){
+    window.alert("Hi")
 }

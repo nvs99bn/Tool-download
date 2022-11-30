78d1cffa6f1a8fe58d6f45eb8d3e280db70d0e84
VSean99 <nvs99bn@gmail.com>
2022-11-30 17:58:57 +0700

    update

--- scripts/openCollection.js
+++ scripts/openCollection.js
@@ -1,19 +1,22 @@
-// let a = [];
-// let getLinks = document
-//   ?.querySelectorAll("figure")
-//   .forEach((link) => a.push(link?.getAttribute("data-image")));
-// localStorage.setItem("links", a);
-// let b = [];
-// document
-//   .querySelectorAll("a > span")
-//   .forEach((a) => b.push(a.innerHTML === "Next page"));
-// console.log(b)
-// let oldData = localStorage.getItem("links").split(",")
+let data = localStorage.getItem("data")?.split(",");
+if (!data) {
+  data = [];
+}
+let links = document?.getElementsByClassName(
+  "showcase__link js-detail-data-link"
+);
+for (let link of links) {
+  data.push(link?.getAttribute("href"));
+}
+localStorage.setItem("data", data);
+
 let linkNextPage = document
-  .getElementsByClassName(
-    "pagination__next button floatl pagination--static pd-y-none-i"
-  )[0]
+  .getElementsByClassName("pagination__next button floatl pd-y-none-i")[0]
   ?.getAttribute("href");
-if(linkNextPage){
-    window.alert("Hi")
+if (linkNextPage) {
+  window.open(linkNextPage);
+  window.close(window.location.href);
+} else {
+  data.forEach((image) => window.open(image))
+  localStorage.removeItem("data");
 }
